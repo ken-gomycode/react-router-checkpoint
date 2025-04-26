4b8eba5986acb0c971173ae7a06e0824d0648207
Ken Ovienadu <kenovienadu@gmail.com>
2025-04-26 21:44:30 +0100

    add Home component to display movie cards with navigation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,15 @@
+import {useNavigate} from "react-router";
+import MovieCard from "../components/MovieCard.tsx";
+import movies from "../movies.json";
+
 const Home = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
+    <div className="p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      {movies.map(movie => (
+        <MovieCard key={movie.slug} movie={movie} onClick={() => navigate(`/movie/${movie.slug}`)} />
+      ))}
     </div>
   );
 }
