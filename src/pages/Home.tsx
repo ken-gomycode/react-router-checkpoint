321259f267944f0947c4bb703f11bde592afdbbc
Ken Ovienadu <kenovienadu@gmail.com>
2025-04-26 21:58:09 +0100

    refactor Home component to improve movie detail navigation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,19 @@
 import {useNavigate} from "react-router";
 import MovieCard from "../components/MovieCard.tsx";
 import movies from "../movies.json";
+import {Movie} from "../types.ts";
 
 const Home = () => {
   const navigate = useNavigate();
 
+  const openMovieDetail = (movie: Movie) => {
+    return () => navigate(`/movie/${movie.slug}`);
+  }
+
   return (
     <div className="p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
       {movies.map(movie => (
-        <MovieCard key={movie.slug} movie={movie} onClick={() => navigate(`/movie/${movie.slug}`)} />
+        <MovieCard key={movie.slug} movie={movie} onClick={openMovieDetail(movie)} />
       ))}
     </div>
   );
